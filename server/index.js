5e9d0a81287a87793a26ee3aa002f322e0980ca4
Tushar Sahu <82487339+TusharSahu02@users.noreply.github.com>
2024-05-24 20:24:50 +0530

    done : deployment

--- server/index.js
+++ server/index.js
@@ -5,17 +5,20 @@ import cookieParser from "cookie-parser";
 
 import connectDB from "./db/connectDB.js";
 
+import { v2 as cloudinary } from "cloudinary";
+import path from "path";
+
 import user from "./routes/user.js";
 import post from "./routes/post.js";
 import url from "./routes/url.js";
 
 dotenv.config();
-
-import { v2 as cloudinary } from "cloudinary";
-
 connectDB();
+
 const app = express();
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 
 cloudinary.config({
   cloud_name: process.env.CLOUDNARY_CLOUD_NAME,
@@ -24,12 +27,12 @@ cloudinary.config({
 });
 
 // Middlewares
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
@@ -38,6 +41,13 @@ app.use(cookieParser());
 app.use("/api/users", user);
 app.use("/api/posts", post);
 app.use("/api/urls", url);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
